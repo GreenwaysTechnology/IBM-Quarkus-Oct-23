6ede430ef170102c88a9521a891c37b737ad3afe
Subramanian Murugan <sasubramanian_md@hotmail.com>
2023-11-02 15:09:33 +0530

    complete orm solutions

--- code-with-quarkus-reactive-orm/src/main/java/com/ibm/reactive/data/activerecord/CustomerResource.java
+++ code-with-quarkus-reactive-orm/src/main/java/com/ibm/reactive/data/activerecord/CustomerResource.java
@@ -1,10 +1,8 @@
 package com.ibm.reactive.data.activerecord;
 
+import io.quarkus.hibernate.reactive.panache.Panache;
 import io.smallrye.mutiny.Uni;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
 
 import java.util.List;
@@ -35,5 +33,45 @@ public class CustomerResource {
             }
             return Response.ok(entity).status(200).build();
         });
+    }
+
+    //POST
+    @POST
+    public Uni<Response> create(Customer customer) {
+        //handle error
+        if (customer == null || customer.name == null) {
+            throw new WebApplicationException("Customer Not Found", 400);
+        }
+        return Panache.withTransaction(customer::persist)
+                .replaceWith(Response.ok(customer)
+                        .status(Response.Status.CREATED).build());
+    }
+
+    @PUT
+    @Path("{id}")
+    public Uni<Response> update(@PathParam("id") Long id, Customer customer) {
+        //handle error
+        if (customer == null || customer.name == null) {
+            throw new WebApplicationException("Customer Name was not set on request.", 422);
+        }
+        return Panache.withTransaction(() ->
+                Customer.<Customer>findById(id).onItem().ifNotNull().invoke(entity -> {
+                            entity.city = customer.city;
+                            entity.name = customer.name;
+                        })
+                        .onItem().ifNotNull().transform(entity -> Response.ok(entity).status(200).build())
+                        .onItem().ifNull().continueWith(Response.ok().status(Response.Status.NOT_FOUND)::build));
+    }
+
+    //Delete
+    @Path("{id}")
+    @DELETE
+    public Uni<Response> delete(@PathParam("id") Long id) {
+        return Panache.withTransaction(() ->
+                Customer.deleteById(id).map(isDeleted ->
+                        isDeleted ? Response.ok().status(Response.Status.NO_CONTENT).build()
+                                : Response.ok().status(Response.Status.NOT_FOUND).build()
+                )
+        );
     }
 }
